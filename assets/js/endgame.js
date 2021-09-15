640eebe2e5f7837800703c6e578e0634c35f4b4f
patricktheodore <patricktheodoresara@gmail.com>
2021-09-15 21:52:12 +0800

    more commenting goodness

--- assets/js/endgame.js
+++ assets/js/endgame.js
@@ -5,20 +5,20 @@ var highScoresListEl = '';
 
 
 
-highScoresListEl.innterHTML=""; 
-for (let i = 0; i < highScores.length; i++) {
+highScoresListEl.innterHTML="";  
+for (let i = 0; i < highScores.length; i++) { //run for loop to display li items with highscores 
     let li = document.createElement('li');
     li.textContent = (highScores[i].name + " - " + highScores[i].score);
-    highScoresList.append(li);
+    highScoresList.append(li); //append them to the page
 };
 
 clearScoresBtn.addEventListener('click', clearScores); 
 
-function clearScores() {
+function clearScores() { //allow user to clear highscore page, by clearing local storage
     localStorage.clear();
     highScoresListEl.innterHTML=""; 
     while (highScoresList.hasChildNodes()) {
-        highScoresList.removeChild(highScoresList.firstChild);
+        highScoresList.removeChild(highScoresList.firstChild); //removes our highscores list first child, whilst it has child Nodes. this removing them all eventually
     }
 };
 
